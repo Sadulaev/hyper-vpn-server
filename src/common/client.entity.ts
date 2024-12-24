d70dc64b3b86ab0333c350a006dcfeed2bf04688
Sadulaev <sadulaevismail1999@gmail.com>
2024-12-24 15:38:39 +0300

    Пофиксил функционал кнопки оформления рассрочки

--- src/common/client.entity.ts
+++ src/common/client.entity.ts
@@ -1,31 +1,37 @@
-import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { User } from "../user/user.entity";
-import { Plan } from "src/common/plan.entity";
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from '../user/user.entity';
+import { Plan } from 'src/common/plan.entity';
 
 @Entity()
 export class Client {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({nullable: false})
-    fullName: string;
+  @Column({ nullable: false })
+  fullName: string;
 
-    @Column({type: 'date', nullable: false})
-    birthDate: Date;
+  @Column({ type: 'date', nullable: false })
+  birthDate: Date;
 
-    @Column({default: null, nullable: true})
-    phone: string | null;
+  @Column({ default: null, nullable: true })
+  phone: string | null;
 
-    // Серия и номер паспорта
-    @Column({nullable: false})
-    serial: string;
+  // Серия и номер паспорта
+  @Column({ nullable: true, default: null })
+  serial: string | null;
 
-    @Column({type: 'json', default: null, nullable: true})
-    images: string | null;
+  @Column({ type: 'json', default: null, nullable: true })
+  images: string | null;
 
-    @ManyToOne(() => User, (user) => user.clients, {nullable: true})
-    user: User | null;
+  @ManyToOne(() => User, (user) => user.clients, { nullable: true })
+  user: User | null;
 
-    @OneToMany(() => Plan, (plan) => plan.client, {nullable: true})
-    plans: Plan[];
+  @OneToMany(() => Plan, (plan) => plan.client, { nullable: true })
+  plans: Plan[];
 }
